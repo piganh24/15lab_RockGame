3942acf8f0bf059b563e5b92fe5d807efb78e13e
unknown <sasha.pigal777@gmail.com>
2025-02-13 23:51:37 +0200

    Update with sum() and range()

--- sum_integer_numbers/main.py
+++ sum_integer_numbers/main.py
@@ -1,14 +1,7 @@
 def sum_numbers_while(n):
     if n <= 0:
         return 0
-    
-    total = 0
-    i = 0
-    for i in range(n + 1):
-        total += i
-        
-
-    return total
+    return sum(i for i in range(n + 1))
 
 num = int(input("Введіть додатнє число: "))
 print("Сума від 0 до ",num ," = ", sum_numbers_while(num))
