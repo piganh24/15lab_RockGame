8662303ad50dc5edff137d2243fab9ab9a175039
unknown <sasha.pigal777@gmail.com>
2025-02-13 23:55:40 +0200

    Add doctest

--- sum_integer_numbers/main.py
+++ sum_integer_numbers/main.py
@@ -1,4 +1,16 @@
 def sum_numbers_for(n):
+    """
+    Обчислює суму чисел від 0 до n.
+
+    >>> sum_numbers_for(5)
+    15
+    >>> sum_numbers_for(0)
+    0
+    >>> sum_numbers_for(-3)
+    0
+    >>> sum_numbers_for("abc")
+    'Помилка: введене значення не є числом!'
+    """
     try:
         n = int(n)
         if n <= 0:
@@ -7,8 +19,12 @@ def sum_numbers_for(n):
     except ValueError:
         return "Помилка: введене значення не є числом!"
 
-try:
-    num = int(input("Введіть додатне ціле число: "))
-    print(f"Сума чисел від 0 до {num} дорівнює {sum_numbers_for(num)}")
-except ValueError:
-    print("Помилка: введене значення не є числом!")
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
+    try:
+        num = int(input("Введіть додатне ціле число: "))
+        print(f"Сума чисел від 0 до {num} дорівнює {sum_numbers_for(num)}")
+    except ValueError:
+        print("Помилка: введене значення не є числом!")
